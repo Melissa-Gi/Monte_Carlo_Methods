d06a23acae5271f3ca3a807c3be3451e9f5bfad7
Melissa Githinji <gthmel001@myuct.ac.za>
2023-08-08 08:43:29 +0200

    Changing run parameters and trying to fix the heap space error

--- src/MonteCarloMini/Run.java
+++ src/MonteCarloMini/Run.java
@@ -3,7 +3,7 @@ public class Run
 {
     public static void main(String [] args)
     {
-        String [] initialConditions = {"10000","1234530","1","1000","1000","10","0.5"};
+        String [] initialConditions = {"10000","1234530","1","1000","10000","10","0.5"};
         MonteCarloMinimizationParallel.main(initialConditions);
     }
 }
